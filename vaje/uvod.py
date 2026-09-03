3ff5546504d99b5db24f98030bccc7efed17fb21
kdornikvoje <klemen.dornik.voje@sts.sckr.si>
2026-09-03 09:59:24 +0000

    poštevanka

--- vaje/uvod.py
+++ vaje/uvod.py
@@ -5,5 +5,13 @@ def hello():
     else:
         print(f"Hello {oddelek}")
 
+def postevanka():
+    stevilo = int(input("Vnesi število: "))
+
+    for i in range(1,11):
+        print(f"{stevilo} x {i} = {stevilo * i}")
+
+
 if __name__ == "__main__":
-    hello()
+    #hello()
+    postevanka()
