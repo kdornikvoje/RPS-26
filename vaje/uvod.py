85cbce3d31a2936399ff9a9df7627dc870564b57
kdornikvoje <klemen.dornik.voje@sts.sckr.si>
2026-09-03 09:45:08 +0000

    uvod.py

--- vaje/uvod.py
+++ vaje/uvod.py
@@ -1,1 +1,9 @@
-print("Hello 1.Ri")
+def hello():
+    oddelek = input("Vnesi oddelek: ")
+    if oddelek.upper() == "1.RI":       
+        print(f"Hello {oddelek} ♥")
+    else:
+        print(f"Hello {oddelek}")
+
+if __name__ == "__main__":
+    hello()
