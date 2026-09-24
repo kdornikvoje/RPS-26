61ad15a4f9e2034d1c8f15ec4a40407becfc6446
kdornikvoje <klemen.dornik.voje@sts.sckr.si>
2026-09-24 09:57:42 +0000

    KŠP 2

--- vaje/kšp.py
+++ vaje/kšp.py
@@ -1,22 +1,60 @@
-def igra(igra:list):
-    while  True:
-        vnosa = input("Vnesi a igralca (K/Š/P): ").upper()
-        vnosb = input("Vnesi b igralca (K/Š/P): ").upper()
-        if vnosa == "" or vnosb =="":
-            break
-        else:
-            igra.append(vnosa)
-            igra.append(vnosb)
-        
-def Rezultati(igra:list):
-    zmagala = 0
-    zmagalb = 0
-    izenaceno = 0
-    IgralecAlast = ""
-    igralecBlast = ""
-    for index  in range(len(igra)):
-        
+def igra(igra):
+    while True:
+        a = input("Igralec A (K/Š/P): ").upper()
+        b = input("Igralec B (K/Š/P): ").upper()
 
-if __name__ =="__main__":
+        if a == "" or b == "":
+            break
+
+        if a != "K" and a != "Š" and a != "P":
+            print("Napačen vnos!")
+            continue
+
+        if b != "K" and b != "Š" and b != "P":
+            print("Napačen vnos!")
+            continue
+
+        igra.append(a)
+        igra.append(b)
+
+    return igra
+
+
+def rezultati(igra):
+    A = 0
+    B = 0
+    izenaceno = 0
+
+    for i in range(0, len(igra), 2):
+        a = igra[i]
+        b = igra[i + 1]
+
+        if a == b:
+            izenaceno = izenaceno + 1
+
+        elif (a == "K" and b == "Š") or \
+             (a == "Š" and b == "P") or \
+             (a == "P" and b == "K"):
+            A = A + 1
+
+        else:
+            B = B + 1
+
+    return A, B, izenaceno
+
+
+def main():
     igraDB = []
+
     igra(igraDB)
+
+    A, B, izenaceno = rezultati(igraDB)
+
+    print("Rezultati:")
+    print("Igralec A:", A)
+    print("Igralec B:", B)
+    print("Izenaceno:", izenaceno)
+
+
+if __name__ == "__main__":
+    main()
